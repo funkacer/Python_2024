a0978ffdb2b8cdc7e8d6275a7de88d51d9ea1382
Jiří Černý <funkacer@gmail.com>
2024-11-09 12:38:55 +0100

    readers data folder correction

--- readers/csv_reader.py
+++ readers/csv_reader.py
@@ -1,7 +1,7 @@
 import csv
 import sys
 
-with open ('_data_csv_reader/Export_Sekce.txt', 'r', encoding='utf-8') as csv_file:
+with open ('_csv_reader_data/Export_Sekce.txt', 'r', encoding='utf-8') as csv_file:
     reader = csv.reader(csv_file, delimiter='\t', quotechar='"') #, quoting=csv.QUOTE_NONNUMERIC)
     while True:
         try:
