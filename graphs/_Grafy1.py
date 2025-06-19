163c17aebad2d4122c5bf307e60f26083eb8731a
Fun Kacer <funkacer@gmail.com>
2025-06-19 06:22:29 +0200

    Grafy from ubuntu

--- graphs/_Grafy1.py
+++ graphs/_Grafy1.py
@@ -18,82 +18,80 @@ def rd(x,y=0):
         result = ''
     return result
 
-def getBarChartH(df, columns, title='Graf', rotation = 45, sort_values = True, sort_ascending = True, limit = 100, precision = 0):
-    '''Tato funkce akceptuje df a nazvy sloupcu, vraci HTML kod horizontalniho grafu'''
+# dodělat možnost na procenta do 100% a label na osu
+def getBarChart(df, columns = [], type='v', precision=0, title='Graf', label='Osa', width=0.8, rotation=0, figsize=(5, 5), html=False):
+    '''Tato funkce vytváří grafy s popisky hodnot'''
     cols = []
-    for i, column in enumerate(columns):
-        #raise error if not found
-        cols.append(df.columns.get_loc(column))
-    if sort_values:
-        df.sort_values(by=df.columns[cols[0]], axis = 0, inplace = True, ascending = sort_ascending)
+    if len(columns) > 0:
+        for i, column in enumerate(columns):
+            #raise error if not found
+            cols.append(df.columns.get_loc(column))
     else:
-        df.sort_index(inplace = True, ascending = sort_ascending)
-    df = df.head(limit)
+        for i, column in enumerate(df.columns):
+            #raise error if not found
+            cols.append(df.columns.get_loc(column))
     ind = np.arange(len(df.index))
-    width = 0.8/len(cols)
-    fig, ax = plt.subplots(figsize = (10,10))
-    rectss = []
-    bottom=np.zeros(len(df.index))
-    for i in range(len(cols)):
-        rectss.append(plt.barh(ind, df[df.columns[cols[i]]], left = bottom, label = df.columns[cols[i]]))
-        bottom += np.array(df[df.columns[cols[i]]])
-    bottom=np.zeros(len(df.index))
-    for rects in rectss:
-        bott = []
-        for i, rect in enumerate(rects):
-            if len(cols) == 1:
-                ax.annotate(s = rd(rect.get_width(),precision), xy = (rect.get_width()+bottom[i], rect.get_y()+rect.get_height()/2), ha = 'left', va = 'center')
+    if type == 'h' or type == 'hr' or type == 'v' or type == 'vr':
+        if type == 'v' or type == 'vr':
+            fig, ax = plt.subplots(figsize = figsize)
+            rectss = []
+            bottom = np.zeros(len(df.index))
+            for i in range(len(cols)):
+                if type == 'v':
+                    rectss.append(plt.bar(x=df.index, height=df[df.columns[cols[i]]], bottom=bottom, width=width, label=df.columns[cols[i]]))
+                    bottom += np.array(df[df.columns[cols[i]]])
+                else:
+                    rectss.append(plt.bar(x=df.index[::-1], height=df[df.columns[cols[i]]][::-1], bottom=bottom, width=width, label=df.columns[cols[i]]))
+                    bottom += np.array(df[df.columns[cols[i]]][::-1])
+            bottom = np.zeros(len(df.index))
+            for rects in rectss:
+                bott = []
+                for i, rect in enumerate(rects):
+                    if len(cols) == 1:
+                        ax.annotate(text=rd(rect.get_height(), precision), xy=(rect.get_x() + rect.get_width()/2, rect.get_height()), ha='center', va='bottom')
+                    else:
+                        ax.annotate(text=rd(rect.get_height(), precision), xy=(rect.get_x() + rect.get_width()/2, rect.get_height()/2 + bottom[i]), ha='center', va='center')
+                    bott.append(rect.get_height())
+                bottom += np.array(bott)
+            if rotation > 0 and rotation < 90:
+                plt.xticks(rotation = rotation, rotation_mode='anchor', ha='right')
             else:
-                ax.annotate(s = rd(rect.get_width(),precision), xy = (rect.get_width()/2+bottom[i], rect.get_y()+rect.get_height()/2), ha = 'center', va = 'center')
-            bott.append(rect.get_width())
-        bottom += np.array(bott)
-    plt.yticks(ind, df.index, rotation = rotation)
-    plt.title(title)
-    ax.legend(loc='upper center', bbox_to_anchor=(1.1, 1),
-          ncol=1, fancybox=True, shadow=True)
-    #plt.show()
-    buf = BytesIO()
-    fig.savefig(buf, format="png", bbox_inches='tight')
-    # Embed the result in the html output.
-    data = base64.b64encode(buf.getbuffer()).decode("ascii")
-    return f"<img src='data:image/png;base64,{data}'/>"
-
-def getBarChartV(df, columns, title='Graf', rotation = 45, sort_values = True, sort_ascending = True, limit = 100, precision = 0):
-    '''Tato funkce akceptuje df a nazvy sloupcu, vraci HTML kod vertikalniho grafu'''
-    cols = []
-    for i, column in enumerate(columns):
-        #raise error if not found
-        cols.append(df.columns.get_loc(column))
-    if sort_values:
-        df.sort_values(by=df.columns[cols[0]], axis = 0, inplace = True, ascending = sort_ascending)
+                plt.xticks(rotation = rotation)
+            plt.ylabel(label)
+        if type == 'h' or type == 'hr':
+            fig, ax = plt.subplots(figsize = figsize)
+            rectss = []
+            bottom = np.zeros(len(df.index))
+            for i in range(len(cols)):
+                if type == 'h':
+                    rectss.append(plt.barh(y=df.index, width=df[df.columns[cols[i]]], left=bottom, height=width, label=df.columns[cols[i]]))
+                    bottom += np.array(df[df.columns[cols[i]]])
+                else:
+                    rectss.append(plt.barh(y=df.index[::-1], width=df[df.columns[cols[i]]][::-1], left=bottom, height=width, label=df.columns[cols[i]]))
+                    bottom += np.array(df[df.columns[cols[i]]][::-1])
+            bottom = np.zeros(len(df.index))
+            for rects in rectss:
+                bott = []
+                for i, rect in enumerate(rects):
+                    if len(cols) == 1:
+                        ax.annotate(text=rd(rect.get_width(),precision), xy=(rect.get_width()+bottom[i], rect.get_y()+rect.get_height()/2), ha='left', va='center')
+                    else:
+                        ax.annotate(text=rd(rect.get_width(),precision), xy=(rect.get_width()/2+bottom[i], rect.get_y()+rect.get_height()/2), ha='center', va='center')
+                    bott.append(rect.get_width())
+                bottom += np.array(bott)
+            if rotation < 0 or rotation > 90:
+                plt.yticks(rotation = rotation, rotation_mode='anchor', ha='right')
+            else:
+                plt.yticks(rotation = rotation)
+            plt.xlabel(label)
+        plt.title(title)
+        ax.legend(loc='upper center', bbox_to_anchor=(1.1, 1), ncol=1, fancybox=True, shadow=True)
+        #plt.show()
+        if html:
+            buf = BytesIO()
+            fig.savefig(buf, format="png", bbox_inches='tight')
+            # Embed the result in the html output.
+            data = base64.b64encode(buf.getbuffer()).decode("ascii")
+            return f"<img src='data:image/png;base64,{data}'/>"
     else:
-        df.sort_index(inplace = True, ascending = sort_ascending)
-    df = df.head(limit)
-    ind = np.arange(len(df.index))
-    width = 0.8/len(cols)
-    fig, ax = plt.subplots(figsize = (10,5))
-    rectss = []
-    bottom=np.zeros(len(df.index))
-    for i in range(len(cols)):
-        rectss.append(plt.bar(ind, df[df.columns[cols[i]]], bottom = bottom, label = df.columns[cols[i]]))
-        bottom += np.array(df[df.columns[cols[i]]])
-    bottom=np.zeros(len(df.index))
-    for rects in rectss:
-        bott = []
-        for i, rect in enumerate(rects):
-            if len(cols) == 1:
-                ax.annotate(s = rd(rect.get_height(),precision), xy = (rect.get_x() + rect.get_width()/2, rect.get_height() + bottom[i]), ha = 'center', va = 'bottom')
-            else:
-                ax.annotate(s = rd(rect.get_height(),precision), xy = (rect.get_x() + rect.get_width()/2, rect.get_height()/2 + bottom[i]), ha = 'center', va = 'center')
-            bott.append(rect.get_height())
-        bottom += np.array(bott)
-    plt.xticks(ind, df.index, rotation = rotation)
-    plt.title(title)
-    ax.legend(loc='upper center', bbox_to_anchor=(1.1, 1),
-          ncol=1, fancybox=True, shadow=True)
-    #plt.show()
-    buf = BytesIO()
-    fig.savefig(buf, format="png", bbox_inches='tight')
-    # Embed the result in the html output.
-    data = base64.b64encode(buf.getbuffer()).decode("ascii")
-    return f"<img src='data:image/png;base64,{data}'/>"
+        raise Exception("Sorry, wrong type(only accept 'h', 'hr', 'v', 'vr')")
